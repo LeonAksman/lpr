14bf396a52119a0bc0facf102be7a84cf334146b
Leon Aksman <leon.aksman@kcl.ac.uk>
2016-05-13 15:47:16 +0100

    Update formAlgoInputStruct_ltcpca.m

--- predictionLib/formAlgoInputStruct_ltcpca.m
+++ predictionLib/formAlgoInputStruct_ltcpca.m
@@ -1,4 +1,19 @@
-%***************************************************************
+%    Form algorithm (classification,regression) input using raw data and training/test indeces, projecting onto LTC-PCA space
+%    Copyright (C) 2016  Leon Aksman
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>
+%
 function [d, featureMat]            = formAlgoInputStruct_ltcpca(dataStruct, trainingIndeces, testIndeces, params)
 
 %make sure they're logically indexed
