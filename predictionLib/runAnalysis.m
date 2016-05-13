b2db6983bb9afa40cca16fbdd32c5a9029b40fdc
Leon Aksman <leon.aksman@kcl.ac.uk>
2016-05-13 15:54:26 +0100

    Update runAnalysis.m

--- predictionLib/runAnalysis.m
+++ predictionLib/runAnalysis.m
@@ -1,3 +1,19 @@
+%    Run the analysis based on the function handles for loading/analyzing/evaluating that come from top level file
+%    Copyright (C) 2016  Leon Aksman
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>
+%
 function metrics                    = runAnalysis(in)
 
 if ~in.DEBUG && fileExist(in.metricsFilename) 
