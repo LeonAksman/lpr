e503a476c06d57d13857299a0844d1622d775045
Leon Aksman <leon.aksman@kcl.ac.uk>
2016-05-13 15:43:22 +0100

    Update demo_ltcpca.m

--- demo_analysis/demo_ltcpca.m
+++ demo_analysis/demo_ltcpca.m
@@ -1,3 +1,19 @@
+%    Demo of an analysis using LTC-PCA, using all (3+) scans from each subject to build projection
+%    Copyright (C) 2016  Leon Aksman
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <http://www.gnu.org/licenses/>
+%
 function metrics                        = demo_ltcpca(runMode, logFilename) 
 
 inDir                                   = '../demo_data';                                         %the directory with where all data lives
